1b7fc3844df30a72734eebce4e87d4c9abc1ff2f
Carlos Hernandez <c.h.corbato@tudelft.nl>
2019-08-05 12:44:58 +0200

    can obtain some inferred facts from swrl rules with pellet

--- owlready_mros_reasoner.py
+++ owlready_mros_reasoner.py
@@ -2,18 +2,18 @@ from owlready2 import *
 
 """
 author: c.h.corbato@tudelft.nl
-using Owlready to manipulate the ontology
-Problem: not possible to access inferred facts in individuals, because it is not supported by Owlready yet
+using Owlready2 to manipulate the ontology
 """
 
 # Load unexmin metacontrol ontology
-onto = get_ontology("file:///home/chcorbato/mros_ws/tomasys/metacontrol_unexmin.owl")
-onto.load()
+onto_path.append("/home/chcorbato/mros_ws/tomasys/") # local folder to search for ontologies
+tomasys = get_ontology("file:///home/chcorbato/mros_ws/tomasys/tomasys.owl").load()
+onto = get_ontology("file:///home/chcorbato/mros_ws/tomasys/abb_scenario1.owl").load()
 
 # UPDATE components statuses
 # TODO: replace using ROS instrocpection
-for i in onto.ComponentState.instances() :
-    if i == onto.motor4:
+for i in list(tomasys.ComponentState.instances()) :
+    if i.iri == 'http://abb_scenario1#c_camera':
         i.c_status = False
     else:
         i.c_status = True
@@ -21,9 +21,29 @@ for i in onto.ComponentState.instances() :
 
 # REASON objective(s) in error
 # sync_reasoner()
-sync_reasoner_pellet()
-for i in onto.Objective.instances() :
-    print(i)
-    # the following does not work because the inferred facts in individuals (property values) cannot be accessed
-    if i.o_status == False:
-        print(i, " o_status= ", i.o_status)
+sync_reasoner_pellet(infer_property_values = True, infer_data_property_values = True)
+
+print("\nComponents Statuses:")
+for i in list(tomasys.ComponentState.instances()) :
+    print(i, i.c_status)
+
+print("\nBindings Statuses:")
+for i in list(tomasys.Binding.instances()) :
+    print(i, i.b_status)
+
+print("\nFG Statuses:")
+for i in list(tomasys.FunctionGrounding.instances()) :
+    print(i, i.b_status)
+
+print("\nObjectives Statuses:")
+for i in list(tomasys.Objective.instances()) :
+    print(i, i.b_status)
+
+print("\nCC availability:")
+for i in list(tomasys.ComponentClass.instances()) :
+    print(i, i.cc_availability)
+
+
+print("\nFD realisability:")
+for i in list(tomasys.FunctionDesign.instances()) :
+    print(i, i.b_status)
