b2f3c50ac71a2e14c256f8aa435a82ef8c5bc78f
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2019-08-16 13:01:14 +0200

    Flesh out skeleton a bit more.

--- mros1_reasoner/scripts/mros1_reasoner.py
+++ mros1_reasoner/scripts/mros1_reasoner.py
@@ -2,10 +2,33 @@
 
 import rospy
 
-if __name__ == '__main__':
-    rospy.init_node('mros1_reasoner', log_level=rospy.DEBUG)
+from cheops_system_state_msgs.msg import SystemState
 
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        rospy.loginfo_throttle(1, "Reasoner node running (dummy) ..")
-        rate.sleep()
+
+sys_state=None
+def callback(msg):
+    global sys_state
+    sys_state = msg
+
+
+def timer_cb(event):
+    global sys_state
+
+    if not sys_state:
+        rospy.logwarn_throttle(1., 'Still waiting on system state ..')
+        return
+
+    rospy.loginfo_throttle(1., 'camera: {}, tag_detect: {}, yumi: {}'.format(
+        sys_state.camera_status, sys_state.tag_detection_status, sys_state.yumi_status))
+
+    # TODO: update reasoner facts, evaluate, retrieve action, publish
+
+
+if __name__ == '__main__':
+    rospy.init_node('mros1_reasoner')
+
+    sub = rospy.Subscriber('system_state', SystemState, callback)
+
+    timer = rospy.Timer(rospy.Duration(1.), timer_cb)
+
+    rospy.spin()
