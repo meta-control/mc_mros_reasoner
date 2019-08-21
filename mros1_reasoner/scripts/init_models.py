f245f963269d068219a3bc36aa3890eb953c23e8
Carlos Hernandez <c.h.corbato@tudelft.nl>
2019-08-21 17:54:18 +0200

    compact initialization of KB elements

--- mros1_reasoner/scripts/init_models.py
+++ mros1_reasoner/scripts/init_models.py
@@ -3,10 +3,10 @@ Initiatize the reasoner Kknowledge base: load ontology and asserts initial state
 '''
 def init_abb_2a(onto, tomasys):
     # Initial system state
-    fd = onto.search(iri = "*fd_build_2arms")[0]
-    f = onto.search(iri = "*f_build_pyramid")[0]
-    yumi = onto.search(iri = "*#c_yumi")[0]
-    role_yumi2 = onto.search(iri = "*r_yumi2a")[0]
+    fd = onto.search_one(iri = "*fd_build_2arms")
+    f = onto.search_one(iri = "*f_build_pyramid")
+    yumi = onto.search_one(iri = "*#c_yumi")[0]
+    role_yumi2 = onto.search_one(iri = "*r_yumi2a")
 
     fg = tomasys.FunctionGrounding("fg_build2", namespace = onto)
     o = tomasys.Objective("o_build_pyramid", namespace = onto)
@@ -21,20 +21,13 @@ def init_abb_2a(onto, tomasys):
 
 def init_abb_2b(onto, tomasys):
     # Initial system state
-    yumi = onto.search(iri = "*#c_yumi")[0]
-    camera = onto.search(iri = "*#c_camera")[0]
-    tag_detector = onto.search(iri = "*#c_tag_detector")[0]
+    yumi = onto.search_one(iri = "*#c_yumi")
+    camera = onto.search_one(iri = "*#c_camera")
+    tag_detector = onto.search_one(iri = "*#c_tag_detector")
 
     #Root objectives
-    o = tomasys.Objective("o_build_pyramid", namespace = onto, typeF=onto.search(iri = "*f_build_pyramid")[0])
+    o = tomasys.Objective("o_build_pyramid", namespace = onto, typeF=onto.search_one(iri = "*f_build_pyramid") )
 
-    # role_yumi2 = onto.search(iri = "*r_yumi2a")[0]
-
-    # fg = tomasys.FunctionGrounding("fg_build2", namespace = onto, typeFD= onto.search(iri = "*fd_build_2arms")[0], realises=o)
-    # b = tomasys.Binding(namespace = onto)
-    # b.binding_component = yumi
-    # b.binding_role = role_yumi2
-    # o.typeF = f
-    # fg.realises = o
-    # fg.typeFD = fd
-    # fg.hasBindings.append(b)
+    # Function Groundings
+    fg = tomasys.FunctionGrounding("fg_build2", namespace = onto, typeFD= onto.search_one(iri = "*fd_build_2arms"), realises=o,
+        hasBindings=[tomasys.Binding(namespace = onto, binding_component = yumi, binding_role=onto.search_one(iri = "*r_yumi2a"))])
