6a4c76f2f7b07b665b011252b36b92daa669a64e
Carlos Hernandez <c.h.corbato@tudelft.nl>
2020-04-20 14:05:53 +0200

    updated to renamed tomasys relations

--- mros1_reasoner/scripts/init_models.py
+++ mros1_reasoner/scripts/init_models.py
@@ -26,18 +26,18 @@ def init_abb_2b(onto, tomasys): # use with abb_dualarm_mm_complete.owl model
     # Function Groundings and Objectives
     o2 = tomasys.Objective("o_tag_localization", namespace = onto, typeF=onto.search_one(iri = "*f_locate_robot_tag") )
     fg = tomasys.FunctionGrounding("fg_build_2arms", namespace = onto, typeFD= onto.search_one(iri = "*fd_build_2arms"),
-            realises = o,
+            solvesO = o,
             needs = [o2],
             hasBindings = [tomasys.Binding(namespace = onto, binding_component = yumi, binding_role = onto.search_one(iri = "*r_yumi2a"))])
     o3 = tomasys.Objective("o_detect_tag", namespace = onto, typeF=onto.search_one(iri = "*f_detect_tag_poses") )
     fg = tomasys.FunctionGrounding("fg_locate_tag", namespace = onto, typeFD= onto.search_one(iri = "*fd_locate_ws1_tag"),
-            realises = o2,
+            solvesO = o2,
             needs = [o3],
             hasBindings = [tomasys.Binding(namespace = onto, binding_component = None, binding_role = onto.search_one(iri = "*r_tag_calibration_node"))])
 
 
     fg = tomasys.FunctionGrounding("fg_detect_tag", namespace = onto, typeFD= onto.search_one(iri = "*fd_detect_tag_poses_1"),
-            realises = o3,
+            solvesO = o3,
             hasBindings = [tomasys.Binding(namespace = onto, binding_component = camera, binding_role = onto.search_one(iri = "*r_camera_fd_detect_tag_poses_1")),
                            tomasys.Binding(namespace = onto, binding_component = tag_detector, binding_role = onto.search_one(iri = "*r_tag_detector_fd_detect_tag_poses_1"))
                             ])
