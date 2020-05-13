1bcd8b9db67789282096c1cbf6003794270d3805
Carlos Hernandez <c.h.corbato@tudelft.nl>
2020-05-13 17:17:09 +0200

    minor fix name

--- mros1_reasoner/scripts/init_models.py
+++ mros1_reasoner/scripts/init_models.py
@@ -45,7 +45,7 @@ def init_abb_2b(onto, tomasys): # use with abb_dualarm_mm_complete.owl model
 
 def init_mvp(onto, tomasys):  # use with mvp.owl model
     # NFRs on QAs
-    nfr_energy = tomasys.QAvalue("nfr_safety", namespace=onto, isQAtype=onto.search_one(
+    nfr_energy = tomasys.QAvalue("nfr_energy", namespace=onto, isQAtype=onto.search_one(
         iri="*energy"), hasValue=1.5)
 
     # Initial system state
