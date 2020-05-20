0a8022676b597f59fb8b6880f3d05955628f8643
Carlos Hernandez <c.h.corbato@tudelft.nl>
2020-05-20 15:43:57 +0200

    removed cheops stuff

--- mros1_reasoner/scripts/mros1_reasoner.py
+++ mros1_reasoner/scripts/mros1_reasoner.py
@@ -111,35 +111,6 @@ def utility(fd):
     return 1
 
 
-# Cheops metacontrol PLAN: returns the FD that has the best efficacy for a given objective o
-def obtainBestEfficacyFunctionDesign(o):
-    global tomasys, onto
-    f = o.typeF
-    # get fds for Function F
-    fds = []
-    for fd in list(tomasys.FunctionDesign.instances()):
-        if fd.solvesF == f:
-            fds.append(fd)
-    print("\nFunctionDesigns available for obj ", o.name, ": ", [fd.name for fd in fds])
-    aux = 0
-    best_fd = None
-    for fd in fds:
-        # FILTER if FD realisability is NOT FALSE (TODO check SWRL rules are complete for this)
-        print("Realisability ", fd.name, fd.fd_realisability)
-        if fd.fd_realisability != False:
-            # FILTER if this objective has already been attempted by the FD
-            # that is the FD error log does NOT contain the current objective
-            print(fd.name, "error_log: ", [i.name for i in fd.fd_error_log])
-            if not o in fd.fd_error_log:
-                if fd.fd_efficacy > aux:
-                    best_fd = fd
-                    aux = fd.fd_efficacy
-    if ( best_fd == None ):
-        print("*** OPERATOR NEEDED, NO SOLUTION FOUND ***")
-        return None
-    else:
-        print("\nBest FD available", best_fd.name)
-        return best_fd
 
 # MVP: select FD to reconfigure to fix Objective in ERROR
 def selectFD(o):
@@ -152,62 +123,6 @@ def selectFD(o):
     else:
         return fd
 
-'''
-CHEOPS To solve the given Objective, recursively Grounds the required hierarchy of
-sub-Objectives and Function Groundings
-'''
-def groundObjective(o, cspecs):
-    global tomasys, onto
-    print("=> Reasoner grounding objective: ", o.name)
-    fd = obtainBestFunctionDesign(o)
-    if( fd == None ):
-        print("*** Objective ", o.name,"cannot be realised ***")
-        return ["safe_shutdown"]
-    fg = tomasys.FunctionGrounding("fg_" + fd.name)
-    print("Roles: ",[r.name for r in fd.roles])
-    for r in fd.roles:
-        b = tomasys.Binding("b_" + r.name)
-        b.binding_role = r
-        fg.has_bindings = b # TODO also ad the binding_component
-        cspecs.append(r.roleDef)
-    for f in fd.requires:
-        print("Requires: ", fd.requires)
-        ob = onto.search_one(typeF = f) # first search if the objective already exists # TODO check that the existing objective also fulfills the req of the FD
-        if ob == None:   # if it does not exist yet, create
-            ob = tomasys.Objective("o_" + f.name)
-            ob.typeF = f
-            fg.needs.append(ob)
-            groundObjective(ob, cspecs)
-        else:
-            fg.needs.append(ob)
-
-    return fg
-
-graph_manipulation_client=None
-last_configuration=["cs_yumi_2"]
-
-# Cheops monitoring received
-def callbackSystemState(msg):
-    sys_state = msg
-    global onto
-
-    if sys_state.yumi_status != 1:
-        c = onto.search_one(iri="*#c_yumi")
-        c.c_status = False
-        print("Yumi in ERROR")
-    if sys_state.camera_status != 1:
-        print("Camera in ERROR")
-        c = onto.search_one(iri="*c_camera")
-        c.c_status = False
-    if sys_state.tag_detection_status != 1:
-        print("Tag not detected")
-        f = onto.search_one(iri="*f_detect_tag_poses")
-        for o in list(tomasys.Objective.instances() ):
-            if (o.typeF == f):
-                o.o_status = "INTERNAL_ERROR"
-
-    rospy.loginfo(1., 'camera: {}, tag_detect: {}, yumi: {}'.format(
-        sys_state.camera_status, sys_state.tag_detection_status, sys_state.yumi_status))
 
 # MVP: callback for diagnostic msg received from QA Observer
 def callbackDiagnostics(msg):
@@ -344,46 +259,6 @@ def timer_cb(event):
         print("-- NO ADAPTATION NEEDED --")
 
 
-# Cheops send reconfiguration goal
-def send_request (reconfiguration_request):
-    global mock
-    goal = GraphManipulationActionGoal()
-    goal.request = reconfiguration_request
-    graph_manipulation_client.send_goal(goal)
-    if mock != True:
-        graph_manipulation_client.wait_for_result()
-        return graph_manipulation_client.get_result().result
-    else:
-        return GraphManipulationMessage.RECONFIGURATION_OK
-
-# for cheops
-spec2request = defaultdict(lambda: GraphManipulationMessage.REQUEST_MISSING, {
-    "cs_yumi1" : GraphManipulationMessage.REQUEST_YUMI_CONFIG_1ARM,
-    "cs_displacement_node" : GraphManipulationMessage.REQUEST_DISPLACEMENT_NODE,
-    "cs_tag_calibration_node" : GraphManipulationMessage.REQUEST_TAG_CALIBRATION_NODE,
-    "cs_camera_1" : GraphManipulationMessage.REQUEST_CAMERA_CONFIG1,
-    "cs_camera_2" : GraphManipulationMessage.REQUEST_CAMERA_CONFIG2,
-    "cs_tag_detector" : GraphManipulationMessage.REQUEST_TAG_DETECTOR,
-    "safe_shutdown" : GraphManipulationMessage.REQUEST_SAFE_SHUTDOWN
-})
-
-# cheops reconfiguration
-def request_reconfiguration(component_specs):
-    rospy.logwarn_throttle(1., 'Reconfiguration requested ..')
-    global last_configuration, spec2request
-    new_specs = filter(lambda cs: cs not in last_configuration, component_specs)
-    requests = map(lambda cs: spec2request[cs], new_specs)
-
-    for r in requests:
-        rospy.logwarn_throttle(1., 'Requesting reconfig: {}'.format(r))
-        result = send_request(r)
-        if (result != GraphManipulationMessage.RECONFIGURATION_OK):
-            last_configuration = []
-            return result
-
-    last_configuration = component_specs
-    return GraphManipulationMessage.RECONFIGURATION_OK
-
 # for MVP with QAs - request the FD.name to reconfigure to
 def request_configuration(fd):
     rospy.logwarn_throttle(1., 'New Configuration requested: {}'.format(fd.name))
@@ -416,19 +291,9 @@ if __name__ == '__main__':
 
     # Start rosnode stuff
     rospy.init_node('mros1_reasoner')
-    sub_system_state = rospy.Subscriber('system_state', SystemState, callbackSystemState)
     sub_diagnostics = rospy.Subscriber('/diagnostics', DiagnosticArray, callbackDiagnostics)
 
-    #for testing YUMI in error
-    # sys_state = SystemState(yumi_status = 1, camera_status = 1, tag_detection_status = 99) # no tag detected
-    # sys_state = SystemState(yumi_status = 1, camera_status = 99, tag_detection_status = 1) # camera error
-
     timer = rospy.Timer(rospy.Duration(3.), timer_cb)
-
-    graph_manipulation_client = actionlib.SimpleActionClient(
-            'cheops_graph_manipulation_action_server',
-            GraphManipulationActionAction)
-    # graph_manipulation_client.wait_for_server()
 
     rosgraph_manipulator_client = actionlib.SimpleActionClient(
         'rosgraph_manipulator_action_server',
