d4d9f57f69c7a34c97cc75669431a7128a32dca1
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2020-05-23 10:23:12 +0200

    list deps of meta-pkg and generates .owl from .rossystem

--- mros1_reasoner/scripts/rosmodel2owl.py
+++ mros1_reasoner/scripts/rosmodel2owl.py
@@ -16,11 +16,24 @@ from rosgraph_monitor.parser import ModelParser
 from pyparsing import ParseResults
 
 from owlready2 import *
+from os import listdir
 
+meta_pack = "metacontrol_move_base_configurations"
 onto_file ='ros_navigation.owl'
 
-ros_root = rospkg.get_ros_root()
 r = rospkg.RosPack()
+
+
+def get_all_files():
+    file_list = []
+    config_pkgs = r.get_depends(meta_pack, implicit=False)
+    for pkg in config_pkgs:
+        path = r.get_path(pkg)
+        for f in listdir(path):
+            if not f.endswith(".rossystem"):
+                continue
+            file_list.append(path + "/" + f)
+    return file_list
 
 onto = None
 # Load ontologies: application model and tomasys
@@ -33,38 +46,33 @@ def loadOntology(file):
 
 import os
 if __name__ == '__main__':
+    rossys_files = get_all_files()
+    # print(rossys_files)
+
     loadOntology(onto_file)
     rospy.loginfo("Loaded domain ontology: " + onto_file)
 
 
     # code to load RosModel and parse it
-    my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(
-        my_path, "../resources/f2_v3_r3.rossystem")
-    print(path)
-    parser = ModelParser(path)
-    print(parser.parse().dump())
+    for file in rossys_files:
+        parser = ModelParser(file)
+        # print(parser.parse().dump())
 
-    model = parser.parse()
-    sys_name = model.system_name[0]
-    safety_attr = model.global_parameters[0]
-    energy_attr = model.global_parameters[1]
-    print(sys_name)
-    print(safety_attr.param_name[0])
-    print(energy_attr.param_name[0])
-    print(safety_attr.param_value[0])
-    print(energy_attr.param_value[0])
+        model = parser.parse()
+        sys_name = model.system_name[0]
+        safety_attr = model.global_parameters[0]
+        energy_attr = model.global_parameters[1]
 
-    # for each RosSystem:
-    # - create a FunctionDesign
-    fd = tomasys.FunctionDesign(sys_name, namespace=onto)
-    # - create a QualityAttribute expected value for the FunctionDesign with the type indicated by the name of the param in the RosSystem and the value of the param (e.g. 0.5)
-    # example:
-    qa = tomasys.QAvalue(energy_attr.param_name[0],
-                         namespace=onto, isQAtype=onto.search_one(iri="*energy"), hasValue=energy_attr.param_value[0])
-    fd.hasQAestimation.append(qa)
-    # END example
+        # for each RosSystem:
+        # - create a FunctionDesign
+        fd = tomasys.FunctionDesign(sys_name, namespace=onto)
+        # - create a QualityAttribute expected value for the FunctionDesign with the type indicated by the name of the param in the RosSystem and the value of the param (e.g. 0.5)
+        # example:
+        qa = tomasys.QAvalue(energy_attr.param_name[0],
+                            namespace=onto, isQAtype=onto.search_one(iri="*energy"), hasValue=energy_attr.param_value[0])
+        fd.hasQAestimation.append(qa)
+        # END example
 
-    # save the ontology to a file
-    onto.save(file="rosmodel.owl", format="rdfxml")
+        # save the ontology to a file
+        onto.save(file="rosmodel.owl", format="rdfxml")
 
