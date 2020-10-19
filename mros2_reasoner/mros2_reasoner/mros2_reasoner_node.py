be861fd43637376dd6cb1bb479a122910c50a5df
Mario Garzon <marioney@gmail.com>
2020-10-19 20:14:28 +0200

    Use a multi threading executor
    Allows calling the service within a timer callback

--- mros2_reasoner/mros2_reasoner/mros2_reasoner_node.py
+++ mros2_reasoner/mros2_reasoner/mros2_reasoner_node.py
@@ -1,23 +1,28 @@
 import rclpy
-import sys
-from threading import Thread
 from mros2_reasoner.ros_reasoner import RosReasoner
+from rclpy.executors import MultiThreadedExecutor
+
 
 def main(args=None):
-    print('Hi from mros2_reasoner.')
+
     # Start rosnode
     rclpy.init(args=args)
+
     ros_reasoner = RosReasoner()
 
-    if ros_reasoner.initialized:
+    # Use a MultiThreadedExecutor to enable processing service request concurrently
+    mt_executor = MultiThreadedExecutor()
+
+    if ros_reasoner.isInitialized is True:
         # initialize KB with the ontology
         ros_reasoner.initKB()
-        rclpy.spin(ros_reasoner)
     else:
         ros_reasoner.get_logger().info("There was an error in the reasoner initialization")
+        return
 
-
-
+    # Spin until the process in terminated
+    rclpy.spin(ros_reasoner, executor=mt_executor)
+    ros_reasoner.destroy_node()
     rclpy.shutdown()
 
 
