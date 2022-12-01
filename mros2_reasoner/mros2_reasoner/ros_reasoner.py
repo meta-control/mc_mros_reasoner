d4d69d039c4dd609248a481eb9d269fd8f3e370a
Gustavo <g.rezendesilva@tudelft.nl>
2022-12-01 18:40:26 +0100

    ♻️  refactor ros_reasoner

--- mros2_reasoner/mros2_reasoner/ros_reasoner.py
+++ mros2_reasoner/mros2_reasoner/ros_reasoner.py
@@ -11,8 +11,6 @@ from diagnostic_msgs.msg import DiagnosticArray
 from diagnostic_msgs.msg import KeyValue
 
 from mros2_reasoner.reasoner import Reasoner
-from mros2_reasoner.tomasys import obtain_best_function_design
-from mros2_reasoner.tomasys import print_ontology_status
 
 from mros2_msgs.action import ControlQos
 from mros2_msgs.msg import QoS
@@ -36,16 +34,20 @@ class RosReasoner(Node, Reasoner):
 
         self.declare_parameter("desired_configuration", Parameter.Type.STRING)
         self.declare_parameter("node_name", "")
-        self.declare_parameter("reasoning_period", 2)
-        self.declare_parameter("use_reconfigure_srv", False)
+        self.declare_parameter("reasoning_period", 5)
+        self.declare_parameter("use_reconfigure_srv", True)
 
         # Whether or not to use system modes reconfiguration
         #  Used mainly for testing
         self.use_reconfiguration_srv = self.get_parameter(
             "use_reconfigure_srv").value
 
+        # Use execute_ros instead of Reasoner.execute
+        if self.use_reconfiguration_srv:
+            self.execute = self.execute_ros
+
         self.is_initialized = False
-        self.mode_change_srv_call_future = None
+        self.mode_change_srv_future = None
         self.req_reconfiguration_result = None
 
         self.node_name = self.get_parameter('node_name').value
@@ -84,21 +86,23 @@ class RosReasoner(Node, Reasoner):
             self.metacontrol_loop_callback,
             callback_group=self.cb_group)
 
-        self.is_initialized = True
+        self.logger = self.get_logger()
+
         # Reasoner initialization completed
-        self.get_logger().info("[RosReasoner] -- Reasoner Initialization Ok")
+        self.is_initialized = True
+        self.logger.info("[RosReasoner] -- Reasoner Initialization Ok")
 
     def set_initial_fd(self, initial_fd):
         if initial_fd != '':
             self.grounded_configuration = initial_fd
-            self.get_logger().info('grounded_configuration initialized to: ' +
-                                   str(self.grounded_configuration))
+            self.logger.info('grounded_configuration initialized to: ' +
+                             str(self.grounded_configuration))
         else:
-            self.get_logger().info('grounded_configuration set to None')
+            self.logger.info('grounded_configuration set to None')
             self.grounded_configuration = None
 
     def objective_cancel_goal_callback(self, cancel_request):
-        self.get_logger().info("Cancel Action Callback!")
+        self.logger.info("Cancel Action Callback!")
         # Stop reasoning
 
         if (cancel_request.qos_expected is None):
@@ -109,22 +113,22 @@ class RosReasoner(Node, Reasoner):
         else:
             if self.remove_objective(
                     cancel_request.qos_expected.objective_id):
-                self.get_logger().info("Objective Cancelled")
+                self.logger.info("Objective Cancelled")
                 return CancelResponse.ACCEPT
             else:
-                self.get_logger().info("Not found")
+                self.logger.info("Not found")
                 return CancelResponse.REJECT
 
     def objective_action_callback(self, objective_handle):
 
-        self.get_logger().info("Objective Action Callback!")
+        self.logger.info("Objective Action Callback!")
         # Stop reasoning
 
+        # TODO: objectives won't have same name
         # Checks if there are previously defined objectives.
         for old_objective in self.search_objectives():
             self.remove_objective(old_objective.name)
 
-        # MonitorObjective.Goal = objective_handle.request.Goal
         obj_created = self.create_objective(objective_handle.request)
         if obj_created:
             while True:
@@ -138,11 +142,13 @@ class RosReasoner(Node, Reasoner):
                         feedback_msg.qos_status.objective_status = str(
                             objective.o_status)
 
-                    feedback_msg.qos_status.objective_type = objective_handle.request.qos_expected.objective_type
+                    feedback_msg.qos_status.objective_type = \
+                        objective_handle.request.qos_expected.objective_type
                     break
                 fg_instance = self.onto.search_one(solvesO=objective)
                 if fg_instance is not None:
-                    feedback_msg.qos_status.selected_mode = fg_instance.typeFD.name
+                    feedback_msg.qos_status.selected_mode = \
+                        fg_instance.typeFD.name
 
                     for qa in fg_instance.hasQAvalue:
                         QAValue = KeyValue()
@@ -167,17 +173,22 @@ class RosReasoner(Node, Reasoner):
         new_objective = self.get_new_tomasys_objective(
             goal_request.qos_expected.objective_id,
             "*" + goal_request.qos_expected.objective_type)
-        self.get_logger().info("Creating Objective {0}".format(new_objective))
-        for nrf_key in goal_request.qos_expected.qos:
-            new_nfr = self.get_new_tomasys_nrf(
-                "nfr_" + nrf_key.key, "*" + nrf_key.key, float(nrf_key.value))
-            self.get_logger().info("Adding NFRs {}".format(new_nfr))
+        self.logger.info("Creating Objective {0}".format(new_objective))
+        for nfr_key in goal_request.qos_expected.qos:
+            nfr_id = \
+                goal_request.qos_expected.objective_id + "_nfr_" + nfr_key.key
+            new_nfr = self.get_new_tomasys_nfr(
+                 nfr_id, "*" + nfr_key.key, float(nfr_key.value))
+            self.logger.info("Adding NFRs {}".format(new_nfr))
             new_objective.hasNFR.append(new_nfr)
+
+        # TODO: this is not working
         if not goal_request.qos_expected.selected_mode:
             self.set_initial_fd(None)
         else:
             self.set_initial_fd(goal_request.qos_expected.selected_mode)
 
+        # TODO: shouldn't this be a swrl rule instead of hardcoded?
         new_objective.o_status = "UNGROUNDED"
 
         return True
@@ -189,176 +200,118 @@ class RosReasoner(Node, Reasoner):
                 # 2 types of diagnostics considered: about bindings in error
                 # (TODO not implemented yet) or about QAs
                 if diagnostic_status.message == "binding error":
-                    self.get_logger().info("binding error received")
+                    self.logger.info("binding error received")
                     up_binding = self.update_binding(diagnostic_status)
                     if up_binding == -1:
-                        self.get_logger().warning(
-                            "Unkown Function Grounding: %s", diagnostic_status.name)
+                        self.logger.warning(
+                            "Unkown Function Grounding: %s",
+                            diagnostic_status.name)
                     elif up_binding == 0:
-                        self.get_logger().warning(
+                        self.logger.warning(
                             "Diagnostics message received for %s with level %d, nothing done about it." %
                             (diagnostic_status.name, diagnostic_status.level))
 
                 # Component error
                 elif diagnostic_status.message == "Component status":
-                    # self.get_logger().warning("Component status value received \tTYPE: {0}\tVALUE: {1}".format(diagnostic_status.values[0].key, diagnostic_status.values[0].value))
+                    # self.logger.warning("Component status value received \tTYPE: {0}\tVALUE: {1}".format(diagnostic_status.values[0].key, diagnostic_status.values[0].value))
                     up_cs = self.update_component_status(
                         diagnostic_status)
                     if up_cs == -1:
-                        self.get_logger().warning("CS message refers to a FG not found in the KB, we asume it refers to the current grounded_configuration (1st fg found in the KB)")
+                        self.logger.warning("CS message refers to a FG not found in the KB, we asume it refers to the current grounded_configuration (1st fg found in the KB)")
                     elif up_cs == 1:
-                        self.get_logger().info(
+                        self.logger.info(
                             "\n\nCS Message received!\tTYPE: {0}\tVALUE: {1}".format(
                                 diagnostic_status.values[0].key,
                                 diagnostic_status.values[0].value))
                     else:
-                        self.get_logger().warning(
+                        self.logger.warning(
                             "Unsupported CS Message received: %s ", str(
                                 diagnostic_status.values[0].key))
 
                 elif diagnostic_status.message == "QA status":
-                    # self.get_logger().warning("QA value received for\t{0} \tTYPE: {1}\tVALUE: {2}".format(diagnostic_status.name, diagnostic_status.values[0].key, diagnostic_status.values[0].value))
+                    # self.logger.warning("QA value received for\t{0} \tTYPE: {1}\tVALUE: {2}".format(diagnostic_status.name, diagnostic_status.values[0].key, diagnostic_status.values[0].value))
                     up_qa = self.update_qa(diagnostic_status)
                     if up_qa == -1:
-                        self.get_logger().warning("QA message refers to a FG not found in the KB, we asume it refers to the current grounded_configuration (1st fg found in the KB)")
+                        self.logger.warning("QA message refers to a FG not found in the KB, we asume it refers to the current grounded_configuration (1st fg found in the KB)")
                     elif up_qa == 1:
-                        self.get_logger().info(
+                        self.logger.info(
                             "QA value received!\tTYPE: {0}\tVALUE: {1}".format(
                                 diagnostic_status.values[0].key,
                                 diagnostic_status.values[0].value))
                     else:
-                        self.get_logger().warning(
+                        self.logger.warning(
                             "Unsupported QA TYPE received: %s ", str(
                                 diagnostic_status.values[0].key))
                 else:
-                    self.get_logger().warning(
+                    self.logger.warning(
                         "Unsupported Message received: %s ", str(
                             diagnostic_status.values[0].key))
 
     # for MVP with QAs - request the FD.name to reconfigure to
-    def request_configuration(self, new_configuration):
-
-        self.get_logger().warning(
-            'New Configuration requested: {}'.format(new_configuration))
+    def request_configuration(self, desired_configuration, objective):
+        self.logger.warning(
+            'New Configuration for objective {0} requested: {1}'.format(
+                objective, desired_configuration))
         self.req_reconfiguration_result = None
 
-        system_modes_cli = self.create_client(
+        system_modes_client = self.create_client(
             ChangeMode,
-            '/' + self.node_name + '/change_mode',
+            '/' + str(objective.typeF.name) + '/change_mode',
             callback_group=self.cb_group)
 
-        while not system_modes_cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().warn('Mode change service /' + self.node_name +
-                                   '/change_mode not available, waiting ...')
-        try:
-            req = ChangeMode.Request()
-            req.mode_name = new_configuration
-            # async call, but the follow up while loop BLOCKS execution till
-            # there is a response
-            mode_change_srv_call_future = system_modes_cli.call_async(req)
-            # try:
-            #     rclpy.spin_until_future_complete(self, mode_change_srv_call_future)
-            #     #call_result = await mode_change_srv_call_future
-            # except Exception as e:
-            #     self.get_logger().info('Service call failed %r' % (e,))
-            # else:
-            #     self.get_logger().info('Result of reconfiguration %d' % (mode_change_srv_call_future.result().success))
-        except Exception as e:
-            self.get_logger().info('Request creation failed %r' % (e,))
-            return None
+        if system_modes_client.wait_for_service(timeout_sec=1.0):
+            try:
+                req = ChangeMode.Request()
+                req.mode_name = desired_configuration
+                mode_change_srv_future = system_modes_client.call_async(req)
+                rclpy.spin_until_future_complete(
+                    self, mode_change_srv_future, timeout_sec=1.0)
+            except Exception as e:
+                self.logger.info('Request creation failed %r' % (e,))
+                return None
+            else:
+                return mode_change_srv_future
         else:
-            return mode_change_srv_call_future
+            self.logger.warning(
+                'Mode change service /' +
+                str(objective.typeF.name) +
+                '/change_mode not available, waiting ...')
+
+    def execute_ros(self, desired_configurations):
+        self.logger.info('  >> Started MAPE-K ** EXECUTION **')
+        for objective in desired_configurations:
+            reconfiguration_result = self.request_configuration(
+                desired_configurations[objective], objective)
+
+            if reconfiguration_result is not None \
+               and reconfiguration_result.success is True:
+                self.logger.info(
+                    'Got Reconfiguration result {}'.format(
+                        reconfiguration_result.success))
+
+                # Process adaptation feedback to update KB:
+                self.set_new_grounding(
+                    desired_configurations[objective], objective)
+            else:
+                self.logger.error("= RECONFIGURATION FAILED =")
+                return
 
     # main metacontrol loop
-    async def metacontrol_loop_callback(self):
+    def metacontrol_loop_callback(self):
 
         if self.is_initialized is not True:
-            self.get_logger().info(
+            self.logger.info(
                 'Waiting to initialize Reasoner -  Nothing else will be done')
             return
-        if self.has_objective() is not True:
-            return
 
-        # PRINT system status
-        print_ontology_status(self.tomasys)
+        # Analyze
+        objectives_in_error = self.analyze()
 
-        self.get_logger().info(
-            '>> Started MAPE-K ** Analysis (ontological reasoning) **')
+        # Plan
+        desired_configurations = self.plan(objectives_in_error)
 
-        # EXEC REASONING to update ontology with inferences
-        if self.perform_reasoning() is False:
-            self.get_logger().error('    >> Reasoning error')
-            self.onto.save(
-                file="error_reasoning.owl", format="rdfxml")
-            return
+        # Execute
+        self.execute(desired_configurations)
 
-        # EVALUATE functional hierarchy (objectives statuses) (MAPE - Analysis)
-        objectives_internal_error = self.get_objectives_in_error()
-        if objectives_internal_error == []:
-            self.get_logger().info(
-                ">> No Objectives in ERROR: no adaptation is needed")
-            return
-        else:
-            for obj_in_error in objectives_internal_error:
-                self.get_logger().warning(
-                    "Objective {0} in status: {1}".format(
-                        obj_in_error.name, obj_in_error.o_status))
-
-        # ADAPT MAPE -Plan & Execute
-        self.get_logger().info('  >> Started MAPE-K ** PLAN adaptation **')
-
-        self.handle_updatable_objectives(objectives_internal_error)
-
-        new_grounded = None
-        # TODO: this seems redundant, set_new_grounding is being called in different places
-        if obj_in_error.o_status in ["UNGROUNDED"] and self.grounded_configuration is not None:
-            self.get_logger().info("  >>  UNGROUNDED objective - Try to set FD {0}".format(
-                self.grounded_configuration))
-            new_grounded = self.set_new_grounding(
-                self.grounded_configuration, obj_in_error)
-
-        if new_grounded is None:
-            self.get_logger().info("  >> Reasoner searches an FD ")
-            new_grounded = obtain_best_function_design(
-                obj_in_error, self.tomasys)
-
-        if new_grounded is None:
-            self.get_logger().error(
-                "No FD found to solve Objective {} ".format(
-                    obj_in_error.name))  # for DEBUGGING in csv
-            self.get_logger().info('Exited metacontrol_loop_callback for metacontrol reasoning')
-            return
-
-        # request new configuration
-        self.get_logger().info('  >> Started MAPE-K ** EXECUTION **')
-
-        if self.use_reconfiguration_srv:
-
-            rec_result = self.request_configuration(new_grounded)
-
-            try:
-                call_result = await rec_result
-            except Exception as e:
-                self.get_logger().info('Reconfiguration request call failed %r' % (e,))
-                return
-
-            self.get_logger().info(
-                'Got Reconfiguration result {}'.format(
-                    call_result.success))
-            # Process adaptation feedback to update KB:
-            if (call_result.success is not None) and (
-                    call_result.success is True):  # reconfiguration executed ok
-                # updates the ontology according to the result of the
-                # adaptation action - destroy fg for Obj and create the newly
-                # grounded one
-                self.grounded_configuration = self.set_new_grounding(
-                    new_grounded, obj_in_error)  # Set new grounded_configuration
-            else:
-                self.get_logger().error("= RECONFIGURATION FAILED =")  # for DEBUGGING in csv
-                return
-        else:
-            self.grounded_configuration = self.set_new_grounding(
-                new_grounded, obj_in_error)  # Set new grounded_configuration
-
-        self.get_logger().info(
-            'Exited metacontrol_loop_callback after successful reconfiguration - Obj set to None')
+        self.logger.info(
+            'Exited metacontrol_loop_callback')
