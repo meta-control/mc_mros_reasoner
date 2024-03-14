ea00b8c80381ed1524f6521403aa9370676d4e0f
Gustavo <g.rezendesilva@tudelft.nl>
2024-03-14 16:19:16 +0100

    ⚡ improve how reasoner is started

--- mros2_reasoner/mros2_reasoner/mros2_reasoner_node.py
+++ mros2_reasoner/mros2_reasoner/mros2_reasoner_node.py
@@ -8,21 +8,26 @@ def main(args=None):
     # Start rosnode
     rclpy.init(args=args)
 
-    ros_reasoner = RosReasoner()
+    try:
+        ros_reasoner = RosReasoner()
+        executor = MultiThreadedExecutor()
+        executor.add_node(ros_reasoner)
 
-    # Use a MultiThreadedExecutor to enable processing service request
-    # concurrently
-    mt_executor = MultiThreadedExecutor()
+        if ros_reasoner.is_initialized is not True:
+            ros_reasoner.get_logger().info(
+                "There was an error in the reasoner initialization")
+            return
 
-    if ros_reasoner.is_initialized is not True:
-        ros_reasoner.get_logger().info(
-            "There was an error in the reasoner initialization")
-        return
-
-    # Spin until the process in terminated
-    rclpy.spin(ros_reasoner, executor=mt_executor)
-    ros_reasoner.destroy_node()
-    rclpy.shutdown()
+        try:
+            executor.spin()
+        except (KeyboardInterrupt, rclpy.executors.ExternalShutdownException):
+            executor.shutdown()
+            ros_reasoner.destroy_node()
+        finally:
+            executor.shutdown()
+            ros_reasoner.destroy_node()
+    finally:
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
